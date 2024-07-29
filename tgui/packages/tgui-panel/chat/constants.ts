358c240cc00806aded6de126de5c4cde6c430f2f
lessthanthree <83487515+lessthnthree@users.noreply.github.com>
2024-07-29 12:58:30 -0700

    effigy debug

--- tgui/packages/tgui-panel/chat/constants.ts
+++ tgui/packages/tgui-panel/chat/constants.ts
@@ -37,6 +37,7 @@ export const MESSAGE_TYPE_EVENTCHAT = 'eventchat';
 export const MESSAGE_TYPE_ADMINLOG = 'adminlog';
 export const MESSAGE_TYPE_ATTACKLOG = 'attacklog';
 export const MESSAGE_TYPE_DEBUG = 'debug';
+export const MESSAGE_TYPE_EFFIGY_DEBUG = 'effigydebug';
 
 // Metadata for each message type
 export const MESSAGE_TYPES = [
@@ -143,6 +144,13 @@ export const MESSAGE_TYPES = [
     type: MESSAGE_TYPE_DEBUG,
     name: 'Debug Log',
     description: 'DEBUG: SSPlanets subsystem Recover().',
+    admin: true,
+  },
+  {
+    type: MESSAGE_TYPE_EFFIGY_DEBUG,
+    name: 'Effigy Debug',
+    description: 'DEBUG: nya.',
+    selector: '.debugyellow, .debugcyan',
     admin: true,
   },
 ];
